13332ca4665ce40ea080c7c9f344fca677313579
krasjet <nil@krj.st>
2020-03-21 01:41:38 -0400

    some more custom settings

--- src/Karasu/Pandoc/Filters/LaTeX/EnvOpts.hs
+++ src/Karasu/Pandoc/Filters/LaTeX/EnvOpts.hs
@@ -58,12 +58,37 @@ lookupPreamble = fromMaybe mathEnv . (flip Map.lookup envTable =<<)
 
 mathEnv :: LaTeXEnv
 mathEnv = [kfmt|\
-\\usepackage{amsmath}
-\\usepackage{amsfonts}\
+\\usepackage{amssymb}
+\\usepackage{amsfonts}
+\\renewcommand*{\\vec}[1]{\\mathbf{#1}}
+\\newcommand*{\\im}{\\ensuremath{{\\mkern0.7mu\\mathrm{i}\\mkern1mu}}}
+\\newcommand*{\\E}{\\ensuremath{{\\mkern1mu\\mathrm{e}\\mkern1mu}}}
+\\newcommand*{\\dif}{\\mathop{}\\!\\mathrm{d}}
+\\newcommand*{\\N}{\\ensuremath{\\mathbb{N}}}
+\\newcommand*{\\Z}{\\ensuremath{\\mathbb{Z}}}
+\\newcommand*{\\Q}{\\ensuremath{\\mathbb{Q}}}
+\\newcommand*{\\R}{\\ensuremath{\\mathbb{R}}}
+\\newcommand*{\\Cplx}{\\ensuremath{\\mathbb{C}}}
+\\newcommand*{\\Trans}{\\ensuremath{{\\mkern-1.5mu\\mathsf{T}}}}
+\\DeclareMathOperator{\\lcm}{lcm}
+\\DeclareMathOperator*{\\argmax}{arg\\,max}
+\\DeclareMathOperator*{\\argmin}{arg\\,min}
+\\renewcommand*{\\star}{{\\mathbin{\\text{\\usefont{OML}{cmm}{m}{it}\\symbol{"3F}}}}}
+\\renewcommand*{\\ast}{{\\mathbin{\\text{\\usefont{OMS}{cmsy}{m}{n}\\symbol{"03}}}}}
+\\DeclareFontEncoding{FML}{}{}
+\\DeclareFontSubstitution{FML}{futm}{m}{it}
+\\renewcommand*{\\theta}{{\\mathord{\\text{\\usefont{FML}{futmi}{m}{it}\\symbol{"12}}}}}
+\\DeclareFontFamily{U}{wncy}{}
+\\DeclareFontShape{U}{wncy}{m}{n}{<<->>wncyr10}{}
+\\newcommand*{\\Shah}{{\\mathord{\\text{\\usefont{U}{wncy}{m}{n}\\symbol{"58}}}}}
+\\makeatletter
+\\renewenvironment{bmatrix}{\\left[\\mkern3mu\\env@matrix}{\\endmatrix\\mkern3mu\\right]}
+\\renewenvironment{vmatrix}{\\left\\lvert\\mkern5mu\\env@matrix}{\\endmatrix\\mkern5mu\\right\\rvert}
+\\makeatother
 |]
 
 tikzEnv :: LaTeXEnv
-tikzEnv = [kfmt|\
+tikzEnv = mathEnv <> [kfmt|\
 \\usepackage{tikz}
-\\usetikzlibrary{positioning}\
+\\usetikzlibrary{arrows,calc,patterns,angles,quotes,3d,arrows.meta,positioning}
 |]
