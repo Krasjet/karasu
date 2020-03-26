dfb22bbed30fba0c46c46486a48c52344357995c
krasjet <nil@krj.st>
2020-03-25 23:00:51 -0400

    some plans

--- src/Karasu/Handlers/Static.hs
+++ src/Karasu/Handlers/Static.hs
@@ -1,4 +1,5 @@
 -- | The route for serving static files
+-- TODO remove this since nginx can handle static files now
 module Karasu.Handlers.Static (StaticFiles) where
 
 import Servant
--- src/Karasu/Handlers/ViewDoc.hs
+++ src/Karasu/Handlers/ViewDoc.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE TypeOperators     #-}
 
 -- | The route for compiled documents
+-- TODO remove this since nginx can handle static files now
 module Karasu.Handlers.ViewDoc (ViewDoc) where
 
 import Servant
